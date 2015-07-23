57d574bc40747ea92d190ca081fa9c3b89fa4ab4
Arcite <kristopher.wagner@outlook.com>
2015-07-22 20:13:46 -0400

    Forms
    tried to make forms, but I suck

--- DnD_Char_form/DnD_Char_form/Form1.cs
+++ DnD_Char_form/DnD_Char_form/Form1.cs
@@ -16,5 +16,37 @@ namespace DnD_Char_form
       {
          InitializeComponent();
       }
+
+      public static string GetTextBoxInput(string sPrompt, string sCaption)
+      {
+         Form fPrompt         = new Form();
+         Label textLabel      = new Label()       {   Left = 50, Top = 20, 
+                                                      Text = sPrompt                   };
+         TextBox textBox      = new TextBox       {   Left = 50, Top = 50,
+                                                      Width = 400                      };
+         Button confirmation  = new Button()       {  Text = "Ok", Left = 350, 
+                                                      Width = 100, Top = 70, 
+                                                      DialogResult = DialogResult.OK   };
+
+         fPrompt.Width           = 500;
+         fPrompt.Height          = 150;
+         fPrompt.FormBorderStyle = FormBorderStyle.FixedDialog;
+         fPrompt.Text            = sCaption;
+         fPrompt.StartPosition   = FormStartPosition.CenterScreen;
+
+         confirmation.Click += (sender, e) => { fPrompt.Close(); };
+
+         fPrompt.Controls.Add(textBox);
+         fPrompt.Controls.Add(confirmation);
+         fPrompt.Controls.Add(textLabel);
+         fPrompt.AcceptButton = confirmation;
+
+         return fPrompt.ShowDialog() == DialogResult.OK ? textBox.Text : "";
+      }
+
+      private void Form1_Load(object sender, EventArgs e)
+      {
+
+      }
    }
 }
